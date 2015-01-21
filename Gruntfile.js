07b6dfbc695003715903743552097fb258850fb3
Bill Brown <bill@echo.co>
2015-01-20 21:26:16 -0600

    integrate EGG system and v2 design

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,15 @@
 'use strict';
-module.exports = function(grunt) {
 
+module.exports = function(grunt) {
 
   // this needs to go at the top - it will print out how long 
   // things took. Helps with debugging
   require('time-grunt')(grunt);
-
+  
   // this allows you to remove all the 'loadNPMtasks' calls, and speeds up task running
-  require('jit-grunt')(grunt);  
+  require('jit-grunt')(grunt, {
+    usebanner: 'grunt-banner',
+  });
 
   // Project configuration.
   grunt.initConfig({
@@ -25,6 +27,160 @@ module.exports = function(grunt) {
       }
     },
 
+    // this task compiles your sass to css using Libsass, a C++ version of sass
+    sass: {
+      dest: {
+        options: {
+            outputStyle: 'compressed'
+        },
+            files: {
+                'build/css/main.css': 'src/sass/main.scss'
+            }
+      },
+    },
+
+    // this task applies vendor prefixes (ie: -webkit, -moz, -o) to your css
+    autoprefixer: {
+      options: {
+        browsers: [
+          '> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', // default support (recommended by autoprefixer)
+          'ie >= 9',
+          'Firefox >= 23', 
+          'Chrome >= 22',
+          'Safari >= 4',
+          'iOS >= 5.1',
+          'Android >= 4.1'
+        ]
+      },
+      custom: {
+        options: {
+          map: true
+        },
+        src: 'build/css/*.css'
+      }
+    },
+
+    uncss: {
+      dist: {
+        files: {
+          'build/css/main.css': ['build/index.html', 'build/project-jart.html']
+        }
+      }
+    },
+
+    csso: {
+      compress: {
+        options: {
+          report: 'gzip'
+        },
+        files: {
+          'build/css/main.css': ['build/css/main.css']
+        }
+      },
+    },
+
+
+    // this task optimizes your images
+    imagemin: {
+      png: {
+        files: [{
+          expand: true,
+          cwd: 'src/img',
+          src: ['**/*.png', '**/*.jpg', '**/*.gif'],
+          dest: 'src/img',
+        }]
+      },
+      svg: {
+        files: [{
+          expand: true,
+          cwd: 'src/img',
+          src: ['**/*.svg'],
+          ext: '.svg',
+          dest: 'src/img',
+        }]
+      },
+    },
+
+    // this tasks copies stuff over
+    copy: {
+      images: {
+        files: [
+          {expand: true, cwd: 'src/img', src: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.gif'], dest: 'build/img/'}
+        ],
+      },
+      // customjs: {
+      //   files: [
+      //     {expand: true, cwd: 'src/js', src: ['scripts.js'], dest: 'build/js/'}
+      //   ],
+      // },
+      cname: {
+        files: [
+          {expand: true, cwd: 'src', src: ['CNAME'], dest: 'build'}
+        ],
+      }
+    },
+
+    // Build HTML from templates and data
+    assemble: {
+      site: {
+        options: {
+          layout: ['src/templates/layouts/default.hbs'],
+          partials: ['src/templates/partials/*.hbs'],
+          data: ['src/templates/data/*.yaml'],
+          prettify: { indent: 2 },
+          flatten: true
+        },
+        files: {
+          'build/': ['src/templates/pages/*.hbs']
+        }
+      }
+    },
+
+    // Before generating new files remove files from previous build.
+    clean: {
+      build: ['tmp/**/*', 'build/**/**']
+    },
+
+    // this task 'watches' files and triggers other grunt tasks when those
+    // files are saved.
+    watch: {
+      sass: {
+        files: ['src/sass/**/**/*.scss'],
+        tasks: ['css']
+      },
+      templates: {
+        files: [
+          'src/templates/pages/*.hbs', 
+          'src/templates/layouts/*.hbs', 
+          'src/templates/partials/*.hbs', 
+          'src/templates/data/*.yaml'
+        ],
+        tasks: ['templates']
+      },
+      js: {
+        files: ['src/js/scripts.js'],
+        tasks: ['javascript']
+      },
+      // this task must come last, and it will refresh your browser (as long
+      // as you have the chrome extension) whenever certain files get changed
+      livereload: {
+        options: { livereload: true },
+        files: [
+          'build/css/style.css',
+          'build/*.html',
+          'build/js/scripts.js'
+        ],
+      }
+    },
+
+    // takana allows for live as-you-type style injection. AWESOME for designing
+    // in the browser.
+    takana: {
+      options: {
+        path: 'src/sass'
+      }
+    },
+
     // this task allows you to publish to github pages
     'gh-pages': {
       options: {
@@ -35,10 +191,25 @@ module.exports = function(grunt) {
 
   });
 
-  // documentation on how to run different tasks is in the readme
   grunt.registerTask('default', ['node_version']);
-  grunt.registerTask('deploy', ['node_version', 'gh-pages']);
+  grunt.registerTask('css', ['node_version', 'sass', 'autoprefixer']);
+  grunt.registerTask('templates', ['node_version', 'assemble']);
+  grunt.registerTask('javascript', ['node_version', 'copy:customjs']);
+  grunt.registerTask('images', ['node_version', 'imagemin', 'copy:images']);
+  grunt.registerTask('fast', ['node_version', 'takana']);
 
+  grunt.registerTask('build', [
+    'node_version',
+    'clean',
+    'imagemin',
+    'assemble',
+    'copy',
+    'sass',
+    'autoprefixer',
+    'uncss',
+    'csso'
+  ]);
 
+  grunt.registerTask('deploy', ['build', 'gh-pages']);
 
 };
